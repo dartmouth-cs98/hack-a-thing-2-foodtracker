77c73545179c7033f61360ad18055dcbb337761c
NikoruChen <shiland@163.com>
2017-09-23 10:57:06 -0400

    unit tested

---
+++ FoodTracker/FoodTracker/Meal.swift
@@ -0,0 +1,42 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Nicole Chen on 9/23/17.
+//
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+    }
+    
+}
+
+
